f77dc9cb5ab0fd43cc923ae7ec86820b4cabc437
Devon McGrath <devon.mcgrath@gmail.com>
2023-07-10 11:48:23 +1200

    Add new links

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 import About from './pages/About';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import BookList from './pages/BookList';
+import Book from './components/Book';
+import NewBook from './components/NewBook';
 
 export const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/books" element={<BookList />} />
+        <Route path="/books/:id" element={<Book />} />
+        <Route path="/books/new" element={<NewBook />} />
+      </Routes>
+    </>
   );
 };
